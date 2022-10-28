ee06db15d1cb18601e41b3f03959ddde23479614
bindubritto <sabbir.ruet.cse>
2022-10-28 15:50:33 +0600

    routee added successfully

---
+++ client/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+import GalaxyWorld from '../components/GalaxyWorld.vue'
+Vue.use(Router)
+
+const  routes = [
+    {
+      path: '/world',
+      name: 'World',
+      component: HelloWorld
+    },
+    {
+      path: '/galaxy',
+      name: 'Galaxy',
+      component: GalaxyWorld
+    }
+  ]
+
+export default new Router({
+    mode: 'history',
+    routes
+})
